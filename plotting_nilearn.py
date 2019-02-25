c690747ae245d12d6e6a27680b1131baf7660626
felenitaribeiro <felenita.ribeiro@gmail.com>
2019-02-25 11:52:47 +1000

    2nd

--- plotting_nilearn.py
+++ plotting_nilearn.py
@@ -6,7 +6,7 @@ import os.path as osp
 import sys
 
 
-path='/home/uqfribe1/PycharmProjects/pytorch_geometric/my_stuff/my_own_data/raw/my_version_retinotopy'
+path='/home/uqfribe1/PycharmProjects/DEEP-fMRI/data/raw/converted'
 curv = scipy.io.loadmat(osp.join(path, 'cifti_curv_all.mat'))['cifti_curv']
 eccentricity = scipy.io.loadmat(osp.join(path, 'cifti_eccentricity_all.mat'))['cifti_eccentricity']
 polarAngle = scipy.io.loadmat(osp.join(path, 'cifti_polarAngle_all.mat'))['cifti_polarAngle']
@@ -36,18 +36,25 @@ eccentricity['x100610_fit1_eccentricity_msmall'][0][0][0:32492][R2['x100610_fit1
 background=np.reshape(curv['x100610_curvature'][0][0][0:32492],(-1))
 
 
-plotting.plot_surf(surf_mesh='/home/uqfribe1/PycharmProjects/pytorch_geometric/my_stuff/data/S1200_7T_Retinotopy_9Zkk/S1200_7T_Retinotopy181/MNINonLinear/fsaverage_LR32k/S1200_7T_Retinotopy181.L.very_inflated_MSMAll.32k_fs_LR.surf.gii',surf_map=np.reshape(eccentricity['x100610_fit1_eccentricity_msmall'][0][0][0:32492],(1,-1)),bg_map=np.reshape(curv['x100610_curvature'][0][0][0:32492],(1,-1)))
+plotting.plot_surf(surf_mesh='/home/uqfribe1/PycharmProjects/DEEP-fMRI/data/raw/original/S1200_7T_Retinotopy_9Zkk/S1200_7T_Retinotopy181/MNINonLinear/fsaverage_LR32k/S1200_7T_Retinotopy181.L.very_inflated_MSMAll.32k_fs_LR.surf.gii',surf_map=np.reshape(eccentricity['x100610_fit1_eccentricity_msmall'][0][0][0:32492],(1,-1)),bg_map=np.reshape(curv['x100610_curvature'][0][0][0:32492],(1,-1)))
 plotting.show()
 
 #for online view
-view=plotting.view_surf(surf_mesh='/home/uqfribe1/PycharmProjects/pytorch_geometric/my_stuff/data/S1200_7T_Retinotopy_9Zkk/S1200_7T_Retinotopy181/MNINonLinear/fsaverage_LR32k/S1200_7T_Retinotopy181.L.midthickness_MSMAll.32k_fs_LR.surf.gii',surf_map=np.reshape(eccentricity['x100610_fit1_eccentricity_msmall'][0][0][0:32492],(-1)),bg_map=background, threshold=threshold,cmap='gist_rainbow_r',black_bg=True,symmetric_cmap=False,vmax=8+threshold)
+view=plotting.view_surf(surf_mesh='/home/uqfribe1/PycharmProjects/DEEP-fMRI/data/raw/original/S1200_7T_Retinotopy_9Zkk/S1200_7T_Retinotopy181/MNINonLinear/fsaverage_LR32k/S1200_7T_Retinotopy181.L.midthickness_MSMAll.32k_fs_LR.surf.gii',surf_map=np.reshape(eccentricity['x100610_fit1_eccentricity_msmall'][0][0][0:32492],(-1)),bg_map=background, threshold=threshold,cmap='gist_rainbow_r',black_bg=True,symmetric_cmap=False,vmax=8+threshold)
 view.open_in_browser()
 
-view=plotting.view_surf(surf_mesh='/home/uqfribe1/PycharmProjects/pytorch_geometric/my_stuff/data/S1200_7T_Retinotopy_9Zkk/S1200_7T_Retinotopy181/MNINonLinear/fsaverage_LR32k/S1200_7T_Retinotopy181.L.midthickness_MSMAll.32k_fs_LR.surf.gii',surf_map=np.reshape(polarAngle['x100610_fit1_polarangle_msmall'][0][0][0:32492],(-1)),bg_map=background, threshold=threshold,cmap='gist_rainbow_r',black_bg=True,symmetric_cmap=False,vmax=360+threshold)
+view=plotting.view_surf(surf_mesh='/home/uqfribe1/PycharmProjects/DEEP-fMRI/data/raw/original/S1200_7T_Retinotopy_9Zkk/S1200_7T_Retinotopy181/MNINonLinear/fsaverage_LR32k/S1200_7T_Retinotopy181.L.midthickness_MSMAll.32k_fs_LR.surf.gii',surf_map=np.reshape(polarAngle['x100610_fit1_polarangle_msmall'][0][0][0:32492],(-1)),bg_map=background, threshold=threshold,cmap='gist_rainbow_r',black_bg=True,symmetric_cmap=False,vmax=360+threshold)
 view.open_in_browser()
 
 
 
-'''view=plotting.view_surf(surf_mesh='/home/uqfribe1/PycharmProjects/pytorch_geometric/my_stuff/data/S1200_7T_Retinotopy_9Zkk/S1200_7T_Retinotopy181/MNINonLinear/fsaverage_LR32k/S1200_7T_Retinotopy181.L.midthickness_MSMAll.32k_fs_LR.surf.gii',surf_map=img.dataobj[0][0:32492],cmap='Dark2')
+'''
+#Plotting the outputs from the neural net
+a=torch.load('model_2examples_v2_output_epoch150.pt')
+pred=a['Measured_values'][299].detach()
+pred=a['Measured_values'][299].detach()
+pred=np.array(pred)+threshold
+view=plotting.view_surf(surf_mesh='/home/uqfribe1/PycharmProjects/DEEP-fMRI/data/raw/original/S1200_7T_Retinotopy_9Zkk/S1200_7T_Retinotopy181/MNINonLinear/fsaverage_LR32k/S1200_7T_Retinotopy181.L.midthickness_MSMAll.32k_fs_LR.surf.gii',surf_map=np.reshape(pred[0:32492],(-1)),cmap='gist_rainbow_r',black_bg=True,symmetric_cmap=False)
+view.open_in_browser()
+'''
 
-view.open_in_browser()'''
