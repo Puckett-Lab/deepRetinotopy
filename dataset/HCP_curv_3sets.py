56cf740e28f50bea36da08b9058c5f869e996ee6
felenitaribeiro <felenita.ribeiro@gmail.com>
2019-02-26 10:36:41 +1000

    2nd

--- dataset/HCP_curv_3sets.py
+++ dataset/HCP_curv_3sets.py
@@ -12,14 +12,20 @@ class Retinotopy(InMemoryDataset):
     url = 'https://balsa.wustl.edu/study/show/9Zkk'
     def __init__(self,
                  root,
-                 train=True,
+                 set=None,
                  transform=None,
                  pre_transform=None,
                  pre_filter=None,
                  n_examples=None):
         self.n_examples = int(n_examples)
         super(Retinotopy, self).__init__(root, transform, pre_transform, pre_filter)
-        path = self.processed_paths[0] if train else self.processed_paths[1]
+        self.set=set
+        if self.set=='Train':
+            path = self.processed_paths[0]
+        elif self.set == 'Development':
+            path = self.processed_paths[1]
+        else:
+            path = self.processed_paths[2]
         self.data, self.slices = torch.load(path)
 
 
@@ -30,7 +36,7 @@ class Retinotopy(InMemoryDataset):
 
     @property
     def processed_file_names(self):
-        return ['training_curv.pt','test_curv.pt']
+        return ['training_curv.pt','development_curv.pt','test_curv.pt']
 
     def download(self):
         raise RuntimeError(
@@ -38,15 +44,22 @@ class Retinotopy(InMemoryDataset):
             'move it to {} and execute SettingDataset.sh'.format(self.url, self.raw_dir))
 
     def process(self):
-        # extract_zip(self.raw_paths[0], self.raw_dir, log=False)
-        path = osp.join(self.raw_dir, 'converted')
+        #extract_zip(self.raw_paths[0], self.raw_dir, log=False)
+        path=osp.join(self.raw_dir, 'converted')
         data_list=[]
         for i in range(0,self.n_examples):
             data=read_HCP(path,Hemisphere='Left',index=i,surface='mid',threshold=2.2)
             if self.pre_transform is not None:
                 data=self.pre_transform(data)
             data_list.append(data)
-        torch.save(self.collate(data_list[:145]),self.processed_paths[0])
-        torch.save(self.collate(data_list[145:]), self.processed_paths[1])
+
+        train=data_list[0:round(len(data_list)*0.6)]
+        dev=data_list[round(len(data_list)*0.6):round(len(data_list)*0.8)]
+        test=data_list[round(len(data_list)*0.8):len(data_list)]
+
+        torch.save(self.collate(train),self.processed_paths[0])
+        torch.save(self.collate(dev), self.processed_paths[1])
+        torch.save(self.collate(test), self.processed_paths[2])
+
 
 
