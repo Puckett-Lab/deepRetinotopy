fff4895c2fc82c24ed8a7c7c36bec39ce449bab8
felenitaribeiro <felenita.ribeiro@gmail.com>
2019-02-27 15:55:24 +1000

    2nd

--- model2_2examples.py
+++ model2_2examples.py
@@ -53,7 +53,7 @@ def train(epoch):
         #print(output_loss.item())
         output_loss.backward()
         optimizer.step()
-    return output_loss
+    return output_loss.detach()
 
 
 def test():
@@ -61,7 +61,7 @@ def test():
     MeanAbsError =0
 
     for data in dev_loader:
-        pred = model(data.to(device))
+        pred = model(data.to(device)).detach()
         threshold=data.to(device).R2.view(-1)>2.2
         y_hat.append(pred)
         y.append(data.to(device).y.view(-1))
