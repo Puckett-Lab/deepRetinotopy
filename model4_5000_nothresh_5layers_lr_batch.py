8826daeab4b714217fd85e609924c25adec845c3
felenitaribeiro <felenita.ribeiro@gmail.com>
2019-03-08 09:32:32 +1000

    2nd

--- model4_5000_nothresh_5layers_lr_batch.py
+++ model4_5000_nothresh_5layers_lr_batch.py
@@ -11,23 +11,26 @@ from torch_geometric.nn import SplineConv
 path=osp.join(osp.dirname(osp.realpath(__file__)),'data')
 pre_transform=T.Compose([T.FaceToEdge()])
 train_dataset=Retinotopy(path,'Train', transform=T.Cartesian(),pre_transform=pre_transform,n_examples=181)
-train_loader=DataLoader(train_dataset[0:2],batch_size=1,shuffle=True)
-dev_loader=DataLoader(train_dataset[0:2],batch_size=1)
+dev_dataset=Retinotopy(path,'Development', transform=T.Cartesian(),pre_transform=pre_transform,n_examples=181)
+train_loader=DataLoader(train_dataset,batch_size=32,shuffle=True)
+dev_loader=DataLoader(train_dataset[0:10],batch_size=1)
 
 class Net(torch.nn.Module):
     def __init__(self):
         super(Net,self).__init__()
         self.conv1=SplineConv(1,8,dim=3,kernel_size=5,norm=False)
-        self.conv2=SplineConv(8,8,dim=3,kernel_size=5,norm=False)
-        self.conv3=SplineConv(8,8,dim=3,kernel_size=5,norm=False)
-        self.conv4 = SplineConv(8, 1, dim=3, kernel_size=5, norm=False)
+        self.conv2=SplineConv(8,16,dim=3,kernel_size=5,norm=False)
+        self.conv3=SplineConv(16,16,dim=3,kernel_size=5,norm=False)
+        self.conv4=SplineConv(16,8,dim=3,kernel_size=5,norm=False)
+        self.conv5 = SplineConv(8, 1, dim=3, kernel_size=5, norm=False)
 
     def forward(self, data):
         x, edge_index, pseudo=data.x,data.edge_index,data.edge_attr
         x=F.elu(self.conv1(x,edge_index,pseudo))
         x = F.elu(self.conv2(x, edge_index, pseudo))
         x = F.elu(self.conv3(x, edge_index, pseudo))
-        x=F.elu(self.conv4(x,edge_index,pseudo)).view(-1)
+        x = F.elu(self.conv4(x, edge_index, pseudo))
+        x=F.elu(self.conv5(x,edge_index,pseudo)).view(-1)
         return x
 
 device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -38,15 +41,15 @@ optimizer=torch.optim.Adam(model.parameters(),lr=0.1)
 def train(epoch):
     model.train()
 
-    if epoch == 150:
+    if epoch == 1000:
         for param_group in optimizer.param_groups:
             param_group['lr'] = 0.05
 
-    if epoch == 250:
+    if epoch == 3000:
         for param_group in optimizer.param_groups:
             param_group['lr'] = 0.01
 
-    if epoch == 350:
+    if epoch == 4000:
         for param_group in optimizer.param_groups:
             param_group['lr'] = 0.005
 
@@ -84,15 +87,15 @@ def test():
 
 
 
-for epoch in range(1, 501):
+for epoch in range(1, 5001):
     loss=train(epoch)
     test_output = test()
     print('Epoch: {:02d}, Train: {:.4f}, Test: {:.4f}'.format(epoch, loss, test_output['MAE']))
-    if epoch%50==0:
-        torch.save({'Epoch':epoch,'Predicted_values':test_output['Predicted_values'],'Measured_values':test_output['Measured_values'],'R2':test_output['R2'],'Loss':loss,'Dev_MAE':test_output['MAE']},osp.join(osp.dirname(osp.realpath(__file__)),'output','model4_2examples_500_nothresh_output_epoch'+str(epoch)+'.pt'))
+    if epoch%1000==0:
+        torch.save({'Epoch':epoch,'Predicted_values':test_output['Predicted_values'],'Measured_values':test_output['Measured_values'],'R2':test_output['R2'],'Loss':loss,'Dev_MAE':test_output['MAE']},osp.join(osp.dirname(osp.realpath(__file__)),'output','model4_5000_nothresh_5layers_lr_batch_output_epoch'+str(epoch)+'.pt'))
     if test_output['MAE']<=10.94: #MeanAbsError from Benson2014
         break
 
 
 #Saving the model's learned parameter and predicted/y values
-torch.save(model.state_dict(),osp.join(osp.dirname(osp.realpath(__file__)),'output','model4_2examples_500_nothresh.pt'))
+torch.save(model.state_dict(),osp.join(osp.dirname(osp.realpath(__file__)),'output','model4_5000_nothresh_5layers_lr_batch.pt'))
