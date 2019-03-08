85eb7fcd6fec77878ede06922b442247efb6639e
felenitaribeiro <felenita.ribeiro@gmail.com>
2019-03-08 10:20:45 +1000

    2nd

--- model4_1000_nothresh_6layers.py
+++ model4_1000_nothresh_6layers.py
@@ -51,7 +51,7 @@ def train(epoch):
         for param_group in optimizer.param_groups:
             param_group['lr'] = 0.01
 
-    if epoch == 3500:
+    if epoch == 700:
         for param_group in optimizer.param_groups:
             param_group['lr'] = 0.005
 
