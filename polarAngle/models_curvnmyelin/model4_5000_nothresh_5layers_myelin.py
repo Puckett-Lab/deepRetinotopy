b2de100218671d1380a3d71184199d0fcac50a60
felenitaribeiro <felenita.ribeiro@gmail.com>
2019-03-19 15:46:12 +1000

    1rst

--- polarAngle/models_curvnmyelin/model4_5000_nothresh_5layers_myelin.py
+++ polarAngle/models_curvnmyelin/model4_5000_nothresh_5layers_myelin.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 import torch_geometric.transforms as T
 
 import sys
-sys.path.append('..')
+sys.path.append('../..')
 
 from dataset.HCP_3sets_visual_nothr import Retinotopy
 from torch_geometric.data import DataLoader
