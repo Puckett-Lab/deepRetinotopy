e69cbbbed01dff16ace7d3bc758f08507471eb4c
felenitaribeiro <felenita.ribeiro@gmail.com>
2019-03-22 14:42:35 +1000

    1rst

--- polarAngle/models_curv/model4_5000_nothresh_5layers_myloss.py
+++ polarAngle/models_curv/model4_5000_nothresh_5layers_myloss.py
@@ -22,7 +22,7 @@ dev_loader=DataLoader(dev_dataset,batch_size=1)
 
 
 def weighted_mse_loss(input, target, weight):
-    return torch.sum(weight * (input - target) ** 2)
+    return torch.mean(weight * (input - target) ** 2)
 
 class Net(torch.nn.Module):
     def __init__(self):
