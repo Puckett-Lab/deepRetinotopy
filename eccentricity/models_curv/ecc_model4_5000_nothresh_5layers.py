b6a99b9489078ab45db97bc31c76419edd7202d4
felenitaribeiro <felenita.ribeiro@gmail.com>
2019-03-22 16:39:25 +1000

    1rst

--- eccentricity/models_curv/ecc_model4_5000_nothresh_5layers.py
+++ eccentricity/models_curv/ecc_model4_5000_nothresh_5layers.py
@@ -17,7 +17,7 @@ pre_transform=T.Compose([T.FaceToEdge()])
 train_dataset=Retinotopy(path,'Train', transform=T.Cartesian(),pre_transform=pre_transform,n_examples=181,prediction='eccentricity')
 dev_dataset=Retinotopy(path,'Development', transform=T.Cartesian(),pre_transform=pre_transform,n_examples=181)
 train_loader=DataLoader(train_dataset,batch_size=16,shuffle=True)
-dev_loader=DataLoader(dev_dataset,batch_size=1)
+dev_loader=DataLoader(dev_dataset,batch_size=1,shuffle=False)
 
 
 class Net(torch.nn.Module):
