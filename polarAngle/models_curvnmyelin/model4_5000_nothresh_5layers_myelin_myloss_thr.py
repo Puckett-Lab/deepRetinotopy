f8e13320a79524e5acda66fa7321e61098f9671b
felenitaribeiro <felenita.ribeiro@gmail.com>
2019-03-25 11:27:19 +1000

    1rst

--- polarAngle/models_curvnmyelin/model4_5000_nothresh_5layers_myelin_myloss_thr.py
+++ polarAngle/models_curvnmyelin/model4_5000_nothresh_5layers_myelin_myloss_thr.py
@@ -49,7 +49,7 @@ optimizer=torch.optim.Adam(model.parameters(),lr=0.1)
 def train(epoch):
     model.train()
 
-    if epoch == 1000:
+    if epoch == 500:
         for param_group in optimizer.param_groups:
             param_group['lr'] = 0.05
 
