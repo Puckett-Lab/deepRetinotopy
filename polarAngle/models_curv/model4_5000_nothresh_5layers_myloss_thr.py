ab57ab20149387b0d9e46e4654715dd09e5ae6fd
felenitaribeiro <felenita.ribeiro@gmail.com>
2019-03-25 14:45:58 +1000

    1rst

--- polarAngle/models_curv/model4_5000_nothresh_5layers_myloss_thr.py
+++ polarAngle/models_curv/model4_5000_nothresh_5layers_myloss_thr.py
@@ -50,7 +50,7 @@ optimizer=torch.optim.Adam(model.parameters(),lr=0.1)
 def train(epoch):
     model.train()
 
-    if epoch == 2000:
+    if epoch == 500:
         for param_group in optimizer.param_groups:
             param_group['lr'] = 0.05
 
