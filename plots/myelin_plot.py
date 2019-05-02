000b560ad335691cdabfeb961ebc3e66cf3158a3
felenitaribeiro <felenita.ribeiro@gmail.com>
2019-05-02 13:53:49 +1000

    1rst

--- plots/myelin_plot.py
+++ plots/myelin_plot.py
@@ -2,15 +2,16 @@ import os.path as osp
 from nilearn import plotting
 import torch_geometric.transforms as T
 import numpy as np
-from functions.def_ROIs import roi
+import matplotlib.pyplot as plt
+from functions.def_ROIs_ROI1 import roi
 
 import sys
 sys.path.append('../..')
 
-from dataset.test_HCP_3sets_visual_nothr_rotated_myelin import Retinotopy
+from dataset.HCP_3sets_visual_nothr_rotated_ROI1 import Retinotopy
 from torch_geometric.data import DataLoader
 
-label_primary_visual_areas = ['V1d', 'V1v', 'V2d', 'V2v', 'V3d', 'V3v']
+label_primary_visual_areas = ['ROI1']
 final_mask_L, final_mask_R, index_L_mask, index_R_mask= roi(label_primary_visual_areas)
 myelin_thr=np.zeros((32492,1))
 
@@ -22,7 +23,7 @@ path=osp.join(osp.dirname(osp.realpath(__file__)),'..','data')
 pre_transform=T.Compose([T.FaceToEdge()])
 train_dataset=Retinotopy(path,'Train', transform=T.Cartesian(),pre_transform=pre_transform,n_examples=181,prediction='polarAngle',myelination=True)
 dev_dataset=Retinotopy(path,'Development', transform=T.Cartesian(),pre_transform=pre_transform,n_examples=181,myelination=True)
-train_loader=DataLoader(train_dataset,batch_size=1,shuffle=True)
+train_loader=DataLoader(train_dataset,batch_size=1,shuffle=False)
 dev_loader=DataLoader(dev_dataset,batch_size=1,shuffle=False)
 
 
@@ -31,10 +32,51 @@ myelin=[]
 for data in train_loader:
     myelin.append(np.array(data.x))
 
-myelin=np.mean(myelin,0)
-
-myelin_thr[final_mask_L==1]=np.reshape(myelin,(-1,1))
+#myelin=np.mean(myelin,0)
 
 
-view=plotting.view_surf(surf_mesh=osp.join(osp.dirname(osp.realpath(__file__)),'..','data/raw/original/S1200_7T_Retinotopy_9Zkk/S1200_7T_Retinotopy181/MNINonLinear/fsaverage_LR32k/S1200_7T_Retinotopy181.L.sphere.32k_fs_LR.surf.gii'),surf_map=np.reshape(myelin_thr[0:32492],(-1)),cmap='gist_rainbow_r',black_bg=True,symmetric_cmap=False)
+
+
+
+#Testing contrast
+
+myelin_transform=np.reshape(myelin,(-1))
+myelin_transform=np.sort(myelin,axis=None)
+
+upper=0.975*len(myelin_transform)
+lower=0.025*len(myelin_transform)
+
+upper=myelin_transform[round(upper)]
+lower=myelin_transform[round(lower)]
+#curv=np.mean(curv,0)
+
+
+
+def transform(input,range):
+    transform=((input-lower)/(upper-lower))*(range-(-range))+(-range)
+    transform[transform>range]=range
+    transform[transform<-range]=-range
+    return transform
+
+#Histogram plots
+myelin=np.reshape(myelin,(-1))
+
+plt.hist(myelin_transform,bins=1000)
+plt.show()
+
+myelin=myelin[0]
+
+myelin_test=transform(myelin,1)
+
+
+
+
+
+
+
+
+myelin_thr[final_mask_L==1]=np.reshape(myelin_test,(-1,1))
+
+
+view=plotting.view_surf(surf_mesh=osp.join(osp.dirname(osp.realpath(__file__)),'..','data/raw/original/S1200_7T_Retinotopy_9Zkk/S1200_7T_Retinotopy181/MNINonLinear/fsaverage_LR32k/S1200_7T_Retinotopy181.L.sphere.32k_fs_LR.surf.gii'),surf_map=np.reshape(myelin_thr[0:32492],(-1)),cmap='gray',black_bg=True,symmetric_cmap=True,vmax=3)
 view.open_in_browser()
