cb33917f0009ed3e2c125f84957818428f59b95b
felenitaribeiro <felenita.ribeiro@gmail.com>
2019-05-07 16:36:03 +1000

    1rst

--- polarAngle/models_contrast/model4_1000_nothresh_rotated_5layers_smoothL1loss_R2_contrast_curvnmyelin_ROI1_range10.py
+++ polarAngle/models_contrast/model4_1000_nothresh_rotated_5layers_smoothL1loss_R2_contrast_curvnmyelin_ROI1_range10.py
@@ -19,7 +19,7 @@ path=osp.join(osp.dirname(osp.realpath(__file__)),'..','..','data')
 pre_transform=T.Compose([T.FaceToEdge()])
 train_dataset=Retinotopy(path,'Train', transform=T.Cartesian(),pre_transform=pre_transform,n_examples=181,prediction='polarAngle',myelination=True)
 dev_dataset=Retinotopy(path,'Development', transform=T.Cartesian(),pre_transform=pre_transform,n_examples=181,prediction='polarAngle',myelination=True)
-train_loader=DataLoader(train_dataset,batch_size=16,shuffle=False)
+train_loader=DataLoader(train_dataset,batch_size=16,shuffle=True)
 dev_loader=DataLoader(dev_dataset,batch_size=1,shuffle=False)
 
 upper_curv=0.36853024
@@ -59,7 +59,6 @@ class Net(torch.nn.Module):
 
     def forward(self, data):
         x, edge_index, pseudo=transform(data.x,10),data.edge_index,data.edge_attr
-        print(x)
         x=F.elu(self.conv1(x,edge_index,pseudo))
         x = F.elu(self.conv2(x, edge_index, pseudo))
         x = F.elu(self.conv3(x, edge_index, pseudo))
