4611f1dd5166860c311db8cd05357fbb40850b12
felenitaribeiro <felenita.ribeiro@gmail.com>
2019-05-07 13:35:52 +1000

    1rst

--- plots/myelin_plot.py
+++ plots/myelin_plot.py
@@ -32,12 +32,12 @@ myelin=[]
 for data in train_loader:
     myelin.append(np.array(data.x))
 
-#myelin=np.mean(myelin,0)
+myelin=np.mean(myelin,0)
 
 
 
 
-
+'''
 #Testing contrast
 
 myelin_transform=np.reshape(myelin,(-1))
@@ -66,7 +66,7 @@ def transform(input,range):
 
 myelin=myelin[0]
 
-myelin_test=transform(myelin,1)
+myelin_test=transform(myelin,1)'''
 
 
 
@@ -74,9 +74,8 @@ myelin_test=transform(myelin,1)
 
 
 
+myelin_thr[final_mask_L==1]=np.reshape(myelin,(-1,1))
 
-myelin_thr[final_mask_L==1]=np.reshape(myelin_test,(-1,1))
 
-
-view=plotting.view_surf(surf_mesh=osp.join(osp.dirname(osp.realpath(__file__)),'..','data/raw/original/S1200_7T_Retinotopy_9Zkk/S1200_7T_Retinotopy181/MNINonLinear/fsaverage_LR32k/S1200_7T_Retinotopy181.L.sphere.32k_fs_LR.surf.gii'),surf_map=np.reshape(myelin_thr[0:32492],(-1)),cmap='gray',black_bg=True,symmetric_cmap=True,vmax=3)
+view=plotting.view_surf(surf_mesh=osp.join(osp.dirname(osp.realpath(__file__)),'..','data/raw/original/S1200_7T_Retinotopy_9Zkk/S1200_7T_Retinotopy181/MNINonLinear/fsaverage_LR32k/S1200_7T_Retinotopy181.L.sphere.32k_fs_LR.surf.gii'),surf_map=np.reshape(myelin_thr[0:32492],(-1)),cmap='gray',black_bg=True,symmetric_cmap=False,vmax=2)
 view.open_in_browser()
