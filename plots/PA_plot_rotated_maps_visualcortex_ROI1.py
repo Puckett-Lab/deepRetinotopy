129e9cf93c5641759f30d8c1b346c395a0784a2f
felenitaribeiro <felenita.ribeiro@gmail.com>
2019-05-09 13:08:46 +1000

    1rst

--- plots/PA_plot_rotated_maps_visualcortex_ROI1.py
+++ plots/PA_plot_rotated_maps_visualcortex_ROI1.py
@@ -20,14 +20,14 @@ measured=np.zeros((32492,1))
 R2_thr=np.zeros((32492,1))
 
 
-a=torch.load('/home/uqfribe1/PycharmProjects/DEEP-fMRI/polarAngle/model4_nothresh_rotated_5layers_smoothL1lossR2_contrast_curv_ROI1_range10_output_epoch1000.pt',map_location='cpu')
-pred[final_mask_L==1]=np.reshape(np.array(a['Predicted_values'][7]),(-1,1))
+a=torch.load('/home/uqfribe1/PycharmProjects/DEEP-fMRI/polarAngle/model4_nothresh_rotated_6layers_smoothL1lossR2_contrast_curv_ROI1_range10_output_epoch1000.pt',map_location='cpu')
+pred[final_mask_L==1]=np.reshape(np.array(a['Predicted_values'][3]),(-1,1))
 
 
 #R2_thr[final_mask_L==1]=np.reshape(np.array(a['R2'][0]),(-1,1))
 #R2_thr=R2_thr<2.2
 
-measured[final_mask_L==1]=np.reshape(np.array(a['Measured_values'][7]),(-1,1))
+measured[final_mask_L==1]=np.reshape(np.array(a['Measured_values'][3]),(-1,1))
 
 pred=np.array(pred)
 minus=pred>180
