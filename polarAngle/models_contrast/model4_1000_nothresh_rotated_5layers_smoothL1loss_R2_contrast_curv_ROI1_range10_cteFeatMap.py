930155c9b1495331cce42415b64487b24bc43116
felenitaribeiro <felenita.ribeiro@gmail.com>
2019-05-13 15:16:27 +1000

    1rst

--- polarAngle/models_contrast/model4_1000_nothresh_rotated_5layers_smoothL1loss_R2_contrast_curv_ROI1_range10_cteFeatMap.py
+++ polarAngle/models_contrast/model4_1000_nothresh_rotated_5layers_smoothL1loss_R2_contrast_curv_ROI1_range10_cteFeatMap.py
@@ -78,7 +78,7 @@ def train(epoch):
         threshold = R2.view(-1) > 2.2
 
         loss=torch.nn.SmoothL1Loss()
-        output_loss=loss(model(data),data.y.view(-1))
+        output_loss=loss(R2*model(data),R2*data.y.view(-1))
         output_loss.backward()
 
         MAE = torch.mean(abs(data.to(device).y.view(-1)[threshold==1] - model(data)[threshold==1])).item()
@@ -116,10 +116,10 @@ for epoch in range(1, 1001):
     test_output = test()
     print('Epoch: {:02d}, Train_loss: {:.4f}, Train_MAE: {:.4f}, Test_MAE: {:.4f}'.format(epoch, loss, MAE,test_output['MAE']))
     if epoch%1000==0:
-        torch.save({'Epoch':epoch,'Predicted_values':test_output['Predicted_values'],'Measured_values':test_output['Measured_values'],'R2':test_output['R2'],'Loss':loss,'Dev_MAE':test_output['MAE']},osp.join(osp.dirname(osp.realpath(__file__)),'..','output','model4_nothresh_rotated_5layers_smoothL1loss_contrast_curv_ROI1_range10_cteFeatMap_output_epoch'+str(epoch)+'.pt'))
+        torch.save({'Epoch':epoch,'Predicted_values':test_output['Predicted_values'],'Measured_values':test_output['Measured_values'],'R2':test_output['R2'],'Loss':loss,'Dev_MAE':test_output['MAE']},osp.join(osp.dirname(osp.realpath(__file__)),'..','output','model4_nothresh_rotated_5layers_smoothL1lossR2_contrast_curv_ROI1_range10_cteFeatMap_output_epoch'+str(epoch)+'.pt'))
     if test_output['MAE']<=10.94: #MeanAbsError from Benson2014
         break
 
 
 #Saving the model's learned parameter and predicted/y values
-torch.save(model.state_dict(),osp.join(osp.dirname(osp.realpath(__file__)),'..','output','model4_nothresh_rotated_5layers_smoothL1loss_contrast_curv_ROI1_range10_cteFeatMap.pt'))
+torch.save(model.state_dict(),osp.join(osp.dirname(osp.realpath(__file__)),'..','output','model4_nothresh_rotated_5layers_smoothL1lossR2_contrast_curv_ROI1_range10_cteFeatMap.pt'))
