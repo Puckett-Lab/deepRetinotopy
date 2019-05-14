54926951ecf3d54ccc67da348607289271b84b4b
felenitaribeiro <felenita.ribeiro@gmail.com>
2019-05-14 15:42:05 +1000

    1rst

--- polarAngle/models_contrast/model4_1000_nothresh_rotated_5layers_smoothL1loss_R2_contrast_curv_ROI1_range10_k25.py
+++ polarAngle/models_contrast/model4_1000_nothresh_rotated_5layers_smoothL1loss_R2_contrast_curv_ROI1_range10_k25.py
@@ -18,7 +18,7 @@ path=osp.join(osp.dirname(osp.realpath(__file__)),'..','..','data')
 pre_transform=T.Compose([T.FaceToEdge()])
 train_dataset=Retinotopy(path,'Train', transform=T.Cartesian(),pre_transform=pre_transform,n_examples=181,prediction='polarAngle',myelination=False)
 dev_dataset=Retinotopy(path,'Development', transform=T.Cartesian(),pre_transform=pre_transform,n_examples=181,prediction='polarAngle',myelination=False)
-train_loader=DataLoader(train_dataset,batch_size=16,shuffle=True)
+train_loader=DataLoader(train_dataset,batch_size=1,shuffle=True)
 dev_loader=DataLoader(dev_dataset,batch_size=1,shuffle=False)
 
 upper_curv=0.36853024
