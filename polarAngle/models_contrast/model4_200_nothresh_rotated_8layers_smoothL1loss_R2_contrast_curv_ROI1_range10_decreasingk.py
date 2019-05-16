f55e6d2567774719fed8b8bc0919a3f09a1308be
felenitaribeiro <felenita.ribeiro@gmail.com>
2019-05-16 12:46:23 +1000

    1rst

--- polarAngle/models_contrast/model4_200_nothresh_rotated_8layers_smoothL1loss_R2_contrast_curv_ROI1_range10_decreasingk.py
+++ polarAngle/models_contrast/model4_200_nothresh_rotated_8layers_smoothL1loss_R2_contrast_curv_ROI1_range10_decreasingk.py
@@ -64,7 +64,7 @@ optimizer=torch.optim.Adam(model.parameters(),lr=0.01)
 def train(epoch):
     model.train()
 
-    if epoch == 500:
+    if epoch == 100:
         for param_group in optimizer.param_groups:
             param_group['lr'] = 0.005
 
@@ -117,11 +117,11 @@ def test():
 
 
 
-for epoch in range(1, 1001):
+for epoch in range(1, 201):
     loss,MAE=train(epoch)
     test_output = test()
     print('Epoch: {:02d}, Train_loss: {:.4f}, Train_MAE: {:.4f}, Test_MAE: {:.4f}'.format(epoch, loss, MAE,test_output['MAE']))
-    if epoch%1000==0:
+    if epoch%200==0:
         torch.save({'Epoch':epoch,'Predicted_values':test_output['Predicted_values'],'Measured_values':test_output['Measured_values'],'R2':test_output['R2'],'Loss':loss,'Dev_MAE':test_output['MAE']},osp.join(osp.dirname(osp.realpath(__file__)),'..','output','model4_nothresh_rotated_8layers_smoothL1lossR2_contrast_curv_ROI1_range10_decreasingk_output_epoch'+str(epoch)+'.pt'))
     if test_output['MAE']<=10.94: #MeanAbsError from Benson2014
         break
