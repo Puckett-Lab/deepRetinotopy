3b277b0474829aad940b7cc3c424495a7ca25177
felenitaribeiro <felenita.ribeiro@gmail.com>
2019-05-21 11:21:34 +1000

    1rst

--- polarAngle/models_contrast/model4_200_nothresh_rotated_6layers_smoothL1loss_R2_contrast_curvnmyelin_ROI1_range10_k10.py
+++ polarAngle/models_contrast/model4_200_nothresh_rotated_6layers_smoothL1loss_R2_contrast_curvnmyelin_ROI1_range10_k10.py
@@ -16,8 +16,8 @@ from torch_geometric.nn import SplineConv
 
 path=osp.join(osp.dirname(osp.realpath(__file__)),'..','..','data')
 pre_transform=T.Compose([T.FaceToEdge()])
-train_dataset=Retinotopy(path,'Train', transform=T.Cartesian(),pre_transform=pre_transform,n_examples=181,prediction='polarAngle',myelination=False)
-dev_dataset=Retinotopy(path,'Development', transform=T.Cartesian(),pre_transform=pre_transform,n_examples=181,prediction='polarAngle',myelination=False)
+train_dataset=Retinotopy(path,'Train', transform=T.Cartesian(),pre_transform=pre_transform,n_examples=181,prediction='polarAngle',myelination=True)
+dev_dataset=Retinotopy(path,'Development', transform=T.Cartesian(),pre_transform=pre_transform,n_examples=181,prediction='polarAngle',myelination=True)
 train_loader=DataLoader(train_dataset,batch_size=1,shuffle=True)
 dev_loader=DataLoader(dev_dataset,batch_size=1,shuffle=False)
 
@@ -25,22 +25,37 @@ upper_curv=0.36853024
 lower_curv=-0.22703196
 
 
-def transform(input,range):
-    transform=(((input-lower_curv)/(upper_curv-lower_curv))*(range-(-range))+(-range))
-    transform[transform>range]=range
-    transform[transform<-range]=-range
-    return transform
+upper_myelin=1.648841
+lower_myelin=1.2585511
 
+
+def transform(input,range):
+    input_T=torch.reshape(input,(-1,2)).transpose(0, 1)
+
+    #Curvature
+    transverse_0=((input_T[0]-lower_curv)/(upper_curv-lower_curv))*(range-(-range))+(-range)
+    transverse_0[input_T[0]>range]=range
+    transverse_0[input_T[0]<-range]=-range
+
+    #Myelin
+    transverse_1 = ((input_T[1] - lower_myelin) / (upper_myelin - lower_myelin)) * (range - (-range)) + (-range)
+    transverse_1[input_T[1] > range] = range
+    transverse_1[input_T[1] < -range] = -range
+
+    transform = torch.cat((torch.reshape(transverse_0,(-1,1)),torch.reshape(transverse_1,(-1,1))),1)
+
+    return transform
 
 
 class Net(torch.nn.Module):
     def __init__(self):
         super(Net,self).__init__()
-        self.conv1=SplineConv(1,256,dim=3,kernel_size=5,norm=False)
-        self.conv2=SplineConv(256,128,dim=3,kernel_size=5,norm=False)
-        self.conv3=SplineConv(128,64,dim=3,kernel_size=5,norm=False)
-        self.conv4=SplineConv(64,16,dim=3,kernel_size=5,norm=False)
-        self.conv5 = SplineConv(16, 1, dim=3, kernel_size=5, norm=False)
+        self.conv1=SplineConv(2,8,dim=3,kernel_size=10,norm=False)
+        self.conv2=SplineConv(8,16,dim=3,kernel_size=10,norm=False)
+        self.conv3=SplineConv(16,16,dim=3,kernel_size=10,norm=False)
+        self.conv4 = SplineConv(16, 16, dim=3, kernel_size=10, norm=False)
+        self.conv5=SplineConv(16,8,dim=3,kernel_size=10,norm=False)
+        self.conv6 = SplineConv(8, 1, dim=3, kernel_size=10, norm=False)
 
     def forward(self, data):
         x, edge_index, pseudo=transform(data.x,10),data.edge_index,data.edge_attr
@@ -48,8 +63,10 @@ class Net(torch.nn.Module):
         x = F.elu(self.conv2(x, edge_index, pseudo))
         x = F.elu(self.conv3(x, edge_index, pseudo))
         x = F.elu(self.conv4(x, edge_index, pseudo))
-        x=F.elu(self.conv5(x,edge_index,pseudo)).view(-1)
+        x = F.elu(self.conv5(x, edge_index, pseudo))
+        x=F.elu(self.conv6(x,edge_index,pseudo)).view(-1)
         return x
+
 
 device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model=Net().to(device)
@@ -112,15 +129,15 @@ def test():
 
 
 
-for epoch in range(1, 1001):
+for epoch in range(1, 201):
     loss,MAE=train(epoch)
     test_output = test()
     print('Epoch: {:02d}, Train_loss: {:.4f}, Train_MAE: {:.4f}, Test_MAE: {:.4f}'.format(epoch, loss, MAE,test_output['MAE']))
-    if epoch%1000==0:
-        torch.save({'Epoch':epoch,'Predicted_values':test_output['Predicted_values'],'Measured_values':test_output['Measured_values'],'R2':test_output['R2'],'Loss':loss,'Dev_MAE':test_output['MAE']},osp.join(osp.dirname(osp.realpath(__file__)),'..','output','model4_nothresh_rotated_5layers_smoothL1lossR2_contrast_curv_ROI1_range10_featmap_output_epoch'+str(epoch)+'.pt'))
+    if epoch%200==0:
+        torch.save({'Epoch':epoch,'Predicted_values':test_output['Predicted_values'],'Measured_values':test_output['Measured_values'],'R2':test_output['R2'],'Loss':loss,'Dev_MAE':test_output['MAE']},osp.join(osp.dirname(osp.realpath(__file__)),'..','output','model4_nothresh_rotated_6layers_smoothL1lossR2_contrast_curvnmyelin_ROI1_range10_k10_output_epoch'+str(epoch)+'.pt'))
     if test_output['MAE']<=10.94: #MeanAbsError from Benson2014
         break
 
 
 #Saving the model's learned parameter and predicted/y values
-torch.save(model.state_dict(),osp.join(osp.dirname(osp.realpath(__file__)),'..','output','model4_nothresh_rotated_5layers_smoothL1lossR2_contrast_curv_ROI1_range10_featmap.pt'))
+torch.save(model.state_dict(),osp.join(osp.dirname(osp.realpath(__file__)),'..','output','model4_nothresh_rotated_6layers_smoothL1lossR2_contrast_curvnmyelin_ROI1_range10_k10.pt'))
