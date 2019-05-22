4606758b6bd837a133b9e93d89ba363c3ab99c8d
felenitaribeiro <felenita.ribeiro@gmail.com>
2019-05-23 08:39:56 +1000

    1rst

--- plots/PA_plot_rotated_maps_visualcortex_ROI1.py
+++ plots/PA_plot_rotated_maps_visualcortex_ROI1.py
@@ -13,6 +13,9 @@ background=np.reshape(curv['x100610_curvature'][0][0][0:32492],(-1))
 
 threshold=1
 
+nocurv=np.isnan(background)
+background[nocurv==1] = 0
+
 label_primary_visual_areas = ['ROI1']
 final_mask_L, final_mask_R, index_L_mask, index_R_mask= roi(label_primary_visual_areas)
 pred=np.zeros((32492,1))
@@ -20,8 +23,8 @@ measured=np.zeros((32492,1))
 R2_thr=np.zeros((32492,1))
 
 
-a=torch.load('/home/uqfribe1/PycharmProjects/DEEP-fMRI/polarAngle/model4_nothresh_rotated_6layers_smoothL1lossR2_contrast_curvnmyelin_ROI1_range10_k25_output_epoch200.pt',map_location='cpu')
-pred[final_mask_L==1]=np.reshape(np.array(a['Predicted_values'][3]),(-1,1))
+a=torch.load('/home/uqfribe1/PycharmProjects/DEEP-fMRI/polarAngle/model4_nothresh_rotated_5layers_smoothL1lossR2_contrast_curvnmyelin_ROI1_range10_k25_output_epoch200.pt',map_location='cpu')
+pred[final_mask_L==1]=np.reshape(np.array(a['Predicted_values'][7]),(-1,1))
 
 
 #R2_thr[final_mask_L==1]=np.reshape(np.array(a['R2'][0]),(-1,1))
@@ -32,8 +35,8 @@ measured[final_mask_L==1]=np.reshape(np.array(a['Measured_values'][3]),(-1,1))
 pred=np.array(pred)
 minus=pred>180
 sum=pred<180
-pred[minus]=pred[minus]-180
-pred[sum]=pred[sum]+180
+pred[minus]=pred[minus]-180+threshold
+pred[sum]=pred[sum]+180+threshold
 pred=np.array(pred)
 #pred[final_mask_L!=1]=-2
 #pred[R2_thr]=0
@@ -41,13 +44,14 @@ pred=np.array(pred)
 measured=np.array(measured)
 minus=measured>180
 sum=measured<180
-measured[minus]=measured[minus]-180
-measured[sum]=measured[sum]+180
+measured[minus]=measured[minus]-180+threshold
+measured[sum]=measured[sum]+180+threshold
 measured=np.array(measured)
 #measured[final_mask_L!=1]=-2
 #measured[R2_thr]=0
 
 
+pred[final_mask_L!=1]=0
 
-view=plotting.view_surf(surf_mesh=osp.join(osp.dirname(osp.realpath(__file__)),'..','data/raw/original/S1200_7T_Retinotopy_9Zkk/S1200_7T_Retinotopy181/MNINonLinear/fsaverage_LR32k/S1200_7T_Retinotopy181.L.sphere.32k_fs_LR.surf.gii'),surf_map=np.reshape(pred[0:32492],(-1)),bg_map=background,cmap='gist_rainbow_r',black_bg=True,symmetric_cmap=False,vmax=360)
+view=plotting.view_surf(surf_mesh=osp.join(osp.dirname(osp.realpath(__file__)),'..','data/raw/original/S1200_7T_Retinotopy_9Zkk/S1200_7T_Retinotopy181/MNINonLinear/fsaverage_LR32k/S1200_7T_Retinotopy181.L.sphere.32k_fs_LR.surf.gii'),surf_map=np.reshape(pred[0:32492],(-1)),bg_map=background,cmap='gist_rainbow_r',black_bg=True,symmetric_cmap=False,threshold=threshold,vmax=361)
 view.open_in_browser()
