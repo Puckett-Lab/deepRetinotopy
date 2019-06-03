9110de14512e704c9071f796f9c8089492f5449b
felenitaribeiro <felenita.ribeiro@gmail.com>
2019-06-03 15:09:43 +1000

    1rst

--- polarAngle/models_contrast/model4_200_nothresh_rotated_12layers_smoothL1loss_R2_lr_curvnmyelin_ROI1_k25_batchnorm_dropout010.py
+++ polarAngle/models_contrast/model4_200_nothresh_rotated_12layers_smoothL1loss_R2_lr_curvnmyelin_ROI1_k25_batchnorm_dropout010.py
@@ -176,10 +176,10 @@ for epoch in range(1, 201):
     test_output = test()
     print('Epoch: {:02d}, Train_loss: {:.4f}, Train_MAE: {:.4f}, Test_MAE: {:.4f}'.format(epoch, loss, MAE,test_output['MAE']))
     if epoch%50==0:
-        torch.save({'Epoch':epoch,'Predicted_values':test_output['Predicted_values'],'Measured_values':test_output['Measured_values'],'R2':test_output['R2'],'Loss':loss,'Dev_MAE':test_output['MAE']},osp.join(osp.dirname(osp.realpath(__file__)),'..','output','model4_nothresh_rotated_12layers_smoothL1lossR2_curvnmyelin_ROI1_k25_batchnorm_dropout010_output_epoch'+str(epoch)+'.pt'))
+        torch.save({'Epoch':epoch,'Predicted_values':test_output['Predicted_values'],'Measured_values':test_output['Measured_values'],'R2':test_output['R2'],'Loss':loss,'Dev_MAE':test_output['MAE']},osp.join(osp.dirname(osp.realpath(__file__)),'..','output','model4_nothresh_rotated_12layers_smoothL1lossR2_lr_curvnmyelin_ROI1_k25_batchnorm_dropout010_output_epoch'+str(epoch)+'.pt'))
     if test_output['MAE']<=10.94: #MeanAbsError from Benson2014
         break
 
 
 #Saving the model's learned parameter and predicted/y values
-torch.save(model.state_dict(),osp.join(osp.dirname(osp.realpath(__file__)),'..','output','model4_nothresh_rotated_12layers_smoothL1lossR2_curvnmyelin_ROI1_k25_batchnorm_dropout010.pt'))
+torch.save(model.state_dict(),osp.join(osp.dirname(osp.realpath(__file__)),'..','output','model4_nothresh_rotated_12layers_smoothL1lossR2_lr_curvnmyelin_ROI1_k25_batchnorm_dropout010.pt'))
