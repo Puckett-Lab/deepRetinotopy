c759b3a7b3bb53e55c9f79692f2ccddcc37a1d44
felenitaribeiro <felenita.ribeiro@gmail.com>
2019-06-03 10:39:39 +1000

    1rst

--- plots/learning_curves.py
+++ plots/learning_curves.py
@@ -3,25 +3,35 @@ import numpy as np
 import os.path as osp
 
 
-text_file=open('/home/uqfribe1/PycharmProjects/DEEP-fMRI/polarAngle/output_8l_batch_models.txt')
+text_file=open('/home/uqfribe1/PycharmProjects/DEEP-fMRI/polarAngle/output_3105-2_2_models.txt')
+#text_file=open('/home/uqfribe1/PycharmProjects/DEEP-fMRI/polarAngle/output_3105-2_2_model16noDO.txt')
 lines=text_file.readlines()
 epochs=[]
-MSE_train=[]
-MSE_test=[]
+MAE_train=[]
+MAE_test=[]
 
 for x in lines:
     epochs.append(x.split(',')[0][7:])
-    MSE_train.append(x.split(',')[2][12:])
-    MSE_test.append(x.split(',')[3][11:][:-1])
+    MAE_train.append(x.split(',')[2][12:])
+    MAE_test.append(x.split(',')[3][11:][:-1])
 text_file.close()
 
-for i in range(len(MSE_train)):
-    MSE_train[i]=float(MSE_train[i])
-    MSE_test[i]=float(MSE_test[i])
+for i in range(len(MAE_train)):
+    MAE_train[i]=float(MAE_train[i])
+    MAE_test[i]=float(MAE_test[i])
 
 plt.figure()
-plt.scatter(np.arange(150),MSE_test[450:],marker='.')
-#plt.scatter(np.arange(150),MSE_train[450:],color='r',marker='.')
-plt.xlim(0,  1000)
-plt.ylim(20, 100)
+#plt.scatter(np.arange(1000),MAE_train[1000:2000],marker='.')
+#plt.scatter(np.arange(1000),MAE_train[3000:4000],marker='.')
+plt.scatter(np.arange(200),MAE_test[1000:1200],marker='.')
+plt.scatter(np.arange(200),MAE_test[3000:3200],color='red',marker='.')
+
+
+#plt.scatter(np.arange(1000),MAE_train[0:1000],marker='.')
+#plt.scatter(np.arange(1000),MAE_train[0:1000],marker='.')
+#plt.scatter(np.arange(1000),MAE_test[0:1000],marker='.')
+#plt.scatter(np.arange(1000),MAE_test[0:1000],color='red',marker='.')
+
+plt.xlim(0,  200)
+plt.ylim(20, 60)
 plt.show()
