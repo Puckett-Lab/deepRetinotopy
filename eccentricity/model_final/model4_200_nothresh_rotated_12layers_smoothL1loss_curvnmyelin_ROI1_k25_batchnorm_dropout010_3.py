82122d8922546166ce115124ad36b83c8c184de3
felenitaribeiro <felenita.ribeiro@gmail.com>
2019-06-25 09:20:50 +1000

    1rst

--- eccentricity/model_final/model4_200_nothresh_rotated_12layers_smoothL1loss_curvnmyelin_ROI1_k25_batchnorm_dropout010_3.py
+++ eccentricity/model_final/model4_200_nothresh_rotated_12layers_smoothL1loss_curvnmyelin_ROI1_k25_batchnorm_dropout010_3.py
@@ -17,8 +17,8 @@ from torch_geometric.nn import SplineConv
 
 path=osp.join(osp.dirname(osp.realpath(__file__)),'..','..','data')
 pre_transform=T.Compose([T.FaceToEdge()])
-train_dataset=Retinotopy(path,'Train', transform=T.Cartesian(),pre_transform=pre_transform,n_examples=181,prediction='polarAngle',myelination=True)
-dev_dataset=Retinotopy(path,'Development', transform=T.Cartesian(),pre_transform=pre_transform,n_examples=181,prediction='polarAngle',myelination=True)
+train_dataset=Retinotopy(path,'Train', transform=T.Cartesian(),pre_transform=pre_transform,n_examples=181,prediction='eccentricity',myelination=True)
+dev_dataset=Retinotopy(path,'Development', transform=T.Cartesian(),pre_transform=pre_transform,n_examples=181,prediction='eccentricity',myelination=True)
 train_loader=DataLoader(train_dataset,batch_size=1,shuffle=True)
 dev_loader=DataLoader(dev_dataset,batch_size=1,shuffle=False)
 
@@ -139,7 +139,7 @@ def train(epoch):
         threshold = R2.view(-1) > 2.2
 
         loss=torch.nn.SmoothL1Loss()
-        output_loss=loss(R2*model(data),R2*data.y.view(-1))
+        output_loss=loss(model(data),data.y.view(-1))
         output_loss.backward()
 
         MAE = torch.mean(abs(data.to(device).y.view(-1)[threshold==1] - model(data)[threshold==1])).item()
@@ -183,13 +183,13 @@ for epoch in range(1, 201):
     test_output = test()
     print('Epoch: {:02d}, Train_loss: {:.4f}, Train_MAE: {:.4f}, Test_MAE: {:.4f}, Test_MAE_thr: {:.4f}'.format(epoch, loss, MAE,test_output['MAE'],test_output['MAE_thr']))
     if epoch%25==0:
-        torch.save({'Epoch':epoch,'Predicted_values':test_output['Predicted_values'],'Measured_values':test_output['Measured_values'],'R2':test_output['R2'],'Loss':loss,'Dev_MAE':test_output['MAE']},osp.join(osp.dirname(osp.realpath(__file__)),'..','output','model4_nothresh_rotated_12layers_smoothL1lossR2_curvnmyelin_ROI1_k25_batchnorm_dropout010_4_output_epoch'+str(epoch)+'.pt'))
-    if test_output['MAE']<=10.94: #MeanAbsError from Benson2014
+        torch.save({'Epoch':epoch,'Predicted_values':test_output['Predicted_values'],'Measured_values':test_output['Measured_values'],'R2':test_output['R2'],'Loss':loss,'Dev_MAE':test_output['MAE']},osp.join(osp.dirname(osp.realpath(__file__)),'..','output','model4_nothresh_ecc_12layers_smoothL1loss_curvnmyelin_ROI1_k25_batchnorm_dropout010_3_output_epoch'+str(epoch)+'.pt'))
+    if test_output['MAE']<=0.3: #MeanAbsError from Benson2014
         break
 
 
 #Saving the model's learned parameter and predicted/y values
-torch.save(model.state_dict(),osp.join(osp.dirname(osp.realpath(__file__)),'..','output','model4_nothresh_rotated_12layers_smoothL1lossR2_curvnmyelin_ROI1_k25_batchnorm_dropout010_4.pt'))
+torch.save(model.state_dict(),osp.join(osp.dirname(osp.realpath(__file__)),'..','output','model4_nothresh_ecc_12layers_smoothL1loss_curvnmyelin_ROI1_k25_batchnorm_dropout010_3.pt'))
 
 end=time.time()
 time=(end-init)/60
