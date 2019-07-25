8bcf59cf95eb83f42903e79b96ee201b1cd571cd
felenitaribeiro <felenita.ribeiro@gmail.com>
2019-07-25 14:30:39 +1000

    1rst

--- plots/distribution_similarity_myelincurv.py
+++ plots/distribution_similarity_myelincurv.py
@@ -7,7 +7,7 @@ import numpy as np
 import sys
 sys.path.append('../..')
 
-from dataset.test_HCP_3sets_visual_nothr_rotated_myelin import Retinotopy
+from dataset.HCP_3sets_visual_nothr_rotated_ROI1 import Retinotopy
 from torch_geometric.data import DataLoader
 
 
@@ -15,39 +15,41 @@ from torch_geometric.data import DataLoader
 
 path=osp.join(osp.dirname(osp.realpath(__file__)),'..','data')
 pre_transform=T.Compose([T.FaceToEdge()])
-train_dataset=Retinotopy(path,'Train', transform=T.Cartesian(),pre_transform=pre_transform,n_examples=181,prediction='polarAngle',myelination=True)
-dev_dataset=Retinotopy(path,'Development', transform=T.Cartesian(),pre_transform=pre_transform,n_examples=181,myelination=True)
+train_dataset=Retinotopy(path,'Train', transform=T.Cartesian(),pre_transform=pre_transform,n_examples=181,prediction='polarAngle',myelination=True,hemisphere='Left')
+dev_dataset=Retinotopy(path,'Development', transform=T.Cartesian(),pre_transform=pre_transform,n_examples=181,prediction='polarAngle',myelination=True,hemisphere='Left')
 train_loader=DataLoader(train_dataset,batch_size=1,shuffle=True)
 dev_loader=DataLoader(dev_dataset,batch_size=1,shuffle=False)
 
 
-
-myelin=[]
+PA=[]
 for data in train_loader:
-    myelin.append(np.array(data.x))
+    PA.append(np.array(data.y))
+
+
+
+similarity_PA=[]
+for i in range(len(PA)):
+    for j in range(len(PA)):
+        if i != j:
+            similarity_PA.append(sci.pearsonr(PA[i],PA[j])[0])
+
+
+myelincurv=[]
+for data in train_loader:
+    myelincurv.append(np.reshape(np.array(data.x).T,(2,-1)))
+
+
+myelin=np.zeros((161,3267))
+curv=np.zeros((161,3267))
+for i in range(161):
+    myelin[i]=myelincurv[i][1]
+    curv[i]=myelincurv[i][0]
 
 similarity_myelin=[]
 for i in range(len(myelin)):
     for j in range(len(myelin)):
         if i != j:
             similarity_myelin.append(sci.pearsonr(myelin[i],myelin[j])[0])
-
-
-
-
-
-path=osp.join(osp.dirname(osp.realpath(__file__)),'..','data')
-pre_transform=T.Compose([T.FaceToEdge()])
-train_dataset=Retinotopy(path,'Train', transform=T.Cartesian(),pre_transform=pre_transform,n_examples=181,prediction='polarAngle',myelination=False)
-dev_dataset=Retinotopy(path,'Development', transform=T.Cartesian(),pre_transform=pre_transform,n_examples=181,myelination=False)
-train_loader=DataLoader(train_dataset,batch_size=1,shuffle=True)
-dev_loader=DataLoader(dev_dataset,batch_size=1,shuffle=False)
-
-
-curv=[]
-for data in train_loader:
-    curv.append(np.array(data.x))
-
 
 similarity_curv=[]
 for i in range(len(myelin)):
@@ -56,12 +58,13 @@ for i in range(len(myelin)):
             similarity_curv.append(sci.pearsonr(curv[i],curv[j])[0])
 
 
-similarity=[]
-for i in range(len(myelin)):
-        similarity.append(sci.pearsonr(myelin[i],curv[i])[0])
 
-similarity=np.reshape(similarity,(-1))
+
+similarity_PA=np.reshape(similarity_PA,(-1))
 similarity_myelin=np.reshape(similarity_myelin,(-1))
 similarity_curv=np.reshape(similarity_curv,(-1))
-plt.boxplot([similarity,similarity_myelin,similarity_curv],[1,2,3],labels=['MyelinxCurv','Myelin','Curvature'])
+print(np.mean(similarity_curv))
+print(np.mean(similarity_PA))
+print(np.mean(similarity_myelin))
+plt.boxplot([similarity_PA,similarity_myelin,similarity_curv],[1,2,3],labels=['PA','Myelin','Curvature'])
 plt.show()
