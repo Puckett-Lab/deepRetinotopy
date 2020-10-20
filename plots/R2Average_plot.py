f6ebfe9d26ca2a22cc7b7eef558743290d7054e3
felenitaribeiro <felenita.ribeiro@gmail.com>
2020-10-20 10:55:20 +1000

    Explained variance map

--- plots/R2Average_plot.py
+++ plots/R2Average_plot.py
@@ -15,16 +15,18 @@ path = '/home/uqfribe1/PycharmProjects/DEEP-fMRI/data/raw/converted'
 curv = scipy.io.loadmat(osp.join(path, 'cifti_curv_all.mat'))['cifti_curv']
 background = np.reshape(curv['x100610_curvature'][0][0][0:32492], (-1))
 
+# Background settings
 threshold = 1
-
 nocurv = np.isnan(background)
 background[nocurv == 1] = 0
 
+# ROI settings
 label_primary_visual_areas = ['ROI']
 final_mask_L, final_mask_R, index_L_mask, index_R_mask = roi(
     label_primary_visual_areas)
 R2_thr = np.zeros((32492, 1))
 
+# Loading data - left hemisphere
 path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data')
 pre_transform = T.Compose([T.FaceToEdge()])
 train_dataset = Retinotopy(path, 'Train', transform=T.Cartesian(),
@@ -43,12 +45,13 @@ test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
 train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True)
 dev_loader = DataLoader(dev_dataset, batch_size=1, shuffle=False)
 
+# Average explained variance map
 R2 = []
 for data in train_loader:
     R2.append(np.array(data.R2))
-
 R2 = np.mean(R2, 0)
 
+# Masking
 R2_thr[final_mask_L == 1] = np.reshape(R2, (-1, 1)) + threshold
 R2_thr[final_mask_L != 1] = 0
 
