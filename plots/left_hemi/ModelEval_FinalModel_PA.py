32f55d1a601998d335bc38d3fa02e34f798d661b
felenitaribeiro <felenita.ribeiro@gmail.com>
2021-01-22 14:31:40 +1000

    scripts for the selection of the final model

--- plots/left_hemi/ModelEval_FinalModel_PA.py
+++ plots/left_hemi/ModelEval_FinalModel_PA.py
@@ -20,15 +20,12 @@ for k in range(len(visual_areas)):
     mean_across = []
 
     for m in range(len(models)):
-        a = torch.load(
-            '/home/uqfribe1/PycharmProjects/DEEP-fMRI/plots/left_hemi/NoTwin'
-            '/model4_nothresh_rotated_12layers_smoothL1lossR2_curvnmyelin_ROI1_k25_batchnorm_dropout010_notwin10_' +
-            models[m] + '_output_epoch200.pt', map_location='cpu')
+        predictions = torch.load(
+            './../../devset_results/Final-pred_Model' +
+            models[m] + '_PA_LH.pt', map_location='cpu')
 
         theta_withinsubj = []
-        theta_acrosssubj = []
         theta_acrosssubj_pred = []
-        theta_acrosssubj_emp = []
 
         label_primary_visual_areas = ['ROI']
         final_mask_L, final_mask_R, index_L_mask, index_R_mask = roi(
@@ -44,20 +41,14 @@ for k in range(len(visual_areas)):
         mask = ROI1 + primary_visual_areas
         mask = mask[ROI1 == 1]
 
-        # Compute angle between predicted and empirical predictions across subj
-        for j in range(len(a['Predicted_values'])):
-            theta_across_temp = []
+        for j in range(len(predictions['Predicted_values'])):
             theta_pred_across_temp = []
-            theta_emp_across_temp = []
-
-            for i in range(len(a['Predicted_values'])):
-                # Compute angle between predicted and empirical predictions
-                # within subj
+            for i in range(len(predictions['Predicted_values'])):
                 if i == j:
                     # Loading predicted values
-                    pred = np.reshape(np.array(a['Predicted_values'][i]),
+                    pred = np.reshape(np.array(predictions['Predicted_values'][i]),
                                       (-1, 1))
-                    measured = np.reshape(np.array(a['Measured_values'][j]),
+                    measured = np.reshape(np.array(predictions['Measured_values'][j]),
                                           (-1, 1))
 
                     # Rescaling polar angles to match the right visual field
@@ -74,23 +65,16 @@ for k in range(len(visual_areas)):
                     measured[sum] = measured[sum] + 180
                     measured = np.array(measured) * (np.pi / 180)
 
-                    # Computing delta theta, angle between vector defined
-                    # predicted value and empirical value same subj
+                    # Computing delta theta
                     theta = smallest_angle(pred, measured)
                     theta_withinsubj.append(theta)
 
                 if i != j:
-                    # Compute angle between predicted and empirical
-                    # predictions across subj
                     # Loading predicted values
-                    pred = np.reshape(np.array(a['Predicted_values'][i]),
+                    pred = np.reshape(np.array(predictions['Predicted_values'][i]),
                                       (-1, 1))
-                    pred2 = np.reshape(np.array(a['Predicted_values'][j]),
+                    pred2 = np.reshape(np.array(predictions['Predicted_values'][j]),
                                        (-1, 1))
-                    measured = np.reshape(np.array(a['Measured_values'][j]),
-                                          (-1, 1))
-                    measured2 = np.reshape(np.array(a['Measured_values'][i]),
-                                           (-1, 1))
 
                     # Rescaling polar angles to match the right visual field
                     # (left hemisphere)
@@ -106,43 +90,14 @@ for k in range(len(visual_areas)):
                     pred2[sum] = pred2[sum] + 180
                     pred2 = np.array(pred2) * (np.pi / 180)
 
-                    minus = measured > 180
-                    sum = measured < 180
-                    measured[minus] = measured[minus] - 180
-                    measured[sum] = measured[sum] + 180
-                    measured = np.array(measured) * (np.pi / 180)
-
-                    minus = measured2 > 180
-                    sum = measured2 < 180
-                    measured2[minus] = measured2[minus] - 180
-                    measured2[sum] = measured2[sum] + 180
-                    measured2 = np.array(measured2) * (np.pi / 180)
-
-                    # # Computing delta theta, angle between vector defined
-                    # predicted i and empirical j map
-                    # theta = smallest_angle(pred,measured)
-                    # theta_across_temp.append(theta)
-                    #
-                    # # Computing delta theta, angle between vector defined
-                    # measured i versus measured j
-                    # theta_emp = smallest_angle(measured,measured2)
-                    # theta_emp_across_temp.append(theta_emp)
-
-                    # Computing delta theta, angle between vector defined
-                    # pred i versus pred j
+                    # Computing delta theta
                     theta_pred = smallest_angle(pred, pred2)
                     theta_pred_across_temp.append(theta_pred)
 
-            # theta_acrosssubj.append(np.mean(theta_across_temp,axis=0))
-            # theta_acrosssubj_emp.append(np.mean(theta_emp_across_temp,
-            # axis=0))
             theta_acrosssubj_pred.append(
                 np.mean(theta_pred_across_temp, axis=0))
 
-        # mean_theta_acrosssubj=np.mean(np.array(theta_acrosssubj),axis=0)
         mean_theta_withinsubj = np.mean(np.array(theta_withinsubj), axis=0)
-        # mean_theta_acrosssubj_emp=np.mean(np.array(theta_acrosssubj_emp),
-        # axis=0)
         mean_theta_acrosssubj_pred = np.mean(np.array(theta_acrosssubj_pred),
                                              axis=0)
 
@@ -154,24 +109,6 @@ for k in range(len(visual_areas)):
 
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
-    # data = np.concatenate(
-    #     [[mean_across[0], len(mean_across[0]) * [models[0]],
-    #     len(mean_across[0]) * ['Between predicted maps']],
-    #      [mean_across[1], len(mean_across[1]) * [models[1]],
-    #      len(mean_across[1]) * ['Between predicted maps']],
-    #      [mean_across[2], len(mean_across[2]) * [models[2]],
-    #      len(mean_across[2]) * ['Between predicted maps']],
-    #      [mean_across[3], len(mean_across[3]) * [models[3]],
-    #      len(mean_across[3]) * ['Between predicted maps']],
-    #      [mean_delta[0], len(mean_across[0]) * [models[0]],
-    #       len(mean_across[0]) * ['Between predicted map and ground truth']],
-    #      [mean_delta[1], len(mean_across[1]) * [models[1]],
-    #       len(mean_across[1]) * ['Between predicted map and ground truth']],
-    #      [mean_delta[2], len(mean_across[2]) * [models[2]],
-    #       len(mean_across[2]) * ['Between predicted map and ground truth']],
-    #      [mean_delta[3], len(mean_across[3]) * [models[3]],
-    #       len(mean_across[3]) * ['Between predicted map and ground
-    #       truth']]], axis=1)
     data = np.concatenate([[mean_across[0], len(mean_across[0]) * [models[0]],
                             len(mean_across[0]) * ['Between predicted maps']],
                            [mean_across[1], len(mean_across[1]) * [models[1]],
@@ -210,11 +147,7 @@ for k in range(len(visual_areas)):
     ax.set_title('Cluster ' + str(k + 1))
     legend = plt.legend()
     legend.remove()
-
     plt.ylim([0, 60])
-    # x = sns.swarmplot(data=mean_delta,color='gray')
-
-    # plt.savefig('PAdif_cluster'+str(k+1)+'.svg')
     plt.show()
 
 # Primary visual cortex
@@ -232,15 +165,12 @@ mean_delta_2 = []
 mean_across_2 = []
 
 for m in range(len(models)):
-    a = torch.load(
-        '/home/uqfribe1/PycharmProjects/DEEP-fMRI/plots/left_hemi'
-        '/model4_nothresh_rotated_12layers_smoothL1lossR2_curvnmyelin_ROI1_k25_batchnorm_dropout010_' +
-        models[m] + '_output_epoch200.pt', map_location='cpu')
+    predictions = torch.load(
+        './../../devset_results/Final-pred_Model' +
+            models[m] + '_PA_LH.pt', map_location='cpu')
 
     theta_withinsubj = []
-    theta_acrosssubj = []
     theta_acrosssubj_pred = []
-    theta_acrosssubj_emp = []
 
     label_primary_visual_areas = ['ROI']
     final_mask_L, final_mask_R, index_L_mask, index_R_mask = roi(
@@ -251,18 +181,13 @@ for m in range(len(models)):
     mask = mask[ROI1 == 1]
 
     # Compute angle between predicted and empirical predictions across subj
-    for j in range(len(a['Predicted_values'])):
-        theta_across_temp = []
+    for j in range(len(predictions['Predicted_values'])):
         theta_pred_across_temp = []
-        theta_emp_across_temp = []
-
-        for i in range(len(a['Predicted_values'])):
-            # Compute angle between predicted and empirical predictions
-            # within subj
+        for i in range(len(predictions['Predicted_values'])):
             if i == j:
                 # Loading predicted values
-                pred = np.reshape(np.array(a['Predicted_values'][i]), (-1, 1))
-                measured = np.reshape(np.array(a['Measured_values'][j]),
+                pred = np.reshape(np.array(predictions['Predicted_values'][i]), (-1, 1))
+                measured = np.reshape(np.array(predictions['Measured_values'][j]),
                                       (-1, 1))
 
                 # Rescaling polar angles to match the right visual field (
@@ -279,21 +204,14 @@ for m in range(len(models)):
                 measured[sum] = measured[sum] + 180
                 measured = np.array(measured) * (np.pi / 180)
 
-                # Computing delta theta, angle between vector defined
-                # predicted value and empirical value same subj
+                # Computing delta theta
                 theta = smallest_angle(pred, measured)
                 theta_withinsubj.append(theta)
 
             if i != j:
-                # Compute angle between predicted and empirical predictions
-                # across subj
                 # Loading predicted values
-                pred = np.reshape(np.array(a['Predicted_values'][i]), (-1, 1))
-                pred2 = np.reshape(np.array(a['Predicted_values'][j]), (-1, 1))
-                measured = np.reshape(np.array(a['Measured_values'][j]),
-                                      (-1, 1))
-                measured2 = np.reshape(np.array(a['Measured_values'][i]),
-                                       (-1, 1))
+                pred = np.reshape(np.array(predictions['Predicted_values'][i]), (-1, 1))
+                pred2 = np.reshape(np.array(predictions['Predicted_values'][j]), (-1, 1))
 
                 # Rescaling polar angles to match the right visual field (
                 # left hemisphere)
@@ -309,40 +227,13 @@ for m in range(len(models)):
                 pred2[sum] = pred2[sum] + 180
                 pred2 = np.array(pred2) * (np.pi / 180)
 
-                minus = measured > 180
-                sum = measured < 180
-                measured[minus] = measured[minus] - 180
-                measured[sum] = measured[sum] + 180
-                measured = np.array(measured) * (np.pi / 180)
-
-                minus = measured2 > 180
-                sum = measured2 < 180
-                measured2[minus] = measured2[minus] - 180
-                measured2[sum] = measured2[sum] + 180
-                measured2 = np.array(measured2) * (np.pi / 180)
-
-                # # Computing delta theta, angle between vector defined
-                # predicted i and empirical j map
-                # theta = smallest_angle(pred,measured)
-                # theta_across_temp.append(theta)
-                #
-                # # Computing delta theta, angle between vector defined
-                # measured i versus measured j
-                # theta_emp = smallest_angle(measured,measured2)
-                # theta_emp_across_temp.append(theta_emp)
-
-                # Computing delta theta, angle between vector defined pred i
-                # versus pred j
+                # Computing delta theta
                 theta_pred = smallest_angle(pred, pred2)
                 theta_pred_across_temp.append(theta_pred)
 
-        # theta_acrosssubj.append(np.mean(theta_across_temp,axis=0))
-        # theta_acrosssubj_emp.append(np.mean(theta_emp_across_temp, axis=0))
         theta_acrosssubj_pred.append(np.mean(theta_pred_across_temp, axis=0))
 
-    # mean_theta_acrosssubj=np.mean(np.array(theta_acrosssubj),axis=0)
     mean_theta_withinsubj = np.mean(np.array(theta_withinsubj), axis=0)
-    # mean_theta_acrosssubj_emp=np.mean(np.array(theta_acrosssubj_emp),axis=0)
     mean_theta_acrosssubj_pred = np.mean(np.array(theta_acrosssubj_pred),
                                          axis=0)
 
@@ -354,29 +245,6 @@ mean_across_2 = np.reshape(np.array(mean_across_2), (5, -1))
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
-# data = np.concatenate([[mean_across_2[0], len(mean_across_2[0])*[models[
-# 0]], len(mean_across_2[0])*['Between predicted maps']],
-#                        [mean_across_2[1], len(mean_across_2[1])*[models[
-#                        1]], len(mean_across_2[1])*['Between predicted
-#                        maps']],
-#                        [mean_across_2[2], len(mean_across_2[2])*[models[
-#                        2]], len(mean_across_2[2])*['Between predicted
-#                        maps']],
-#                        [mean_across_2[3], len(mean_across_2[3])*[models[
-#                        3]], len(mean_across_2[3])*['Between predicted
-#                        maps']],
-#                        [mean_delta_2[0], len(mean_across_2[0])*[models[
-#                        0]], len(mean_across_2[0])*['Between predicted map
-#                        and ground truth']],
-#                        [mean_delta_2[1], len(mean_across_2[1])*[models[
-#                        1]], len(mean_across_2[1])*['Between predicted map
-#                        and ground truth']],
-#                        [mean_delta_2[2], len(mean_across_2[2])*[models[
-#                        2]], len(mean_across_2[2])*['Between predicted map
-#                        and ground truth']],
-#                        [mean_delta_2[3], len(mean_across_2[3])*[models[
-#                        3]], len(mean_across_2[3])*['Between predicted map
-#                        and ground truth']]], axis=1)
 data = np.concatenate([[mean_across_2[0], len(mean_across_2[0]) * [models[0]],
                         len(mean_across_2[0]) * ['Between predicted maps']],
                        [mean_across_2[1], len(mean_across_2[1]) * [models[1]],
@@ -415,9 +283,5 @@ ax = sns.pointplot(y='$\Delta$$\t\Theta$', x='models', order=models,
 ax.set_title('Early visual cortex ')
 legend = plt.legend()
 legend.remove()
-
 plt.ylim([0, 60])
-# x = sns.swarmplot(data=mean_delta,color='gray')
-
-# plt.savefig('PAdif_cluster'+str(k+1)+'.svg')
 plt.show()
