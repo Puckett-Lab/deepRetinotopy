8d5ee3677c88495ef5766cf4057d11a693e0ca4f
felenitaribeiro <felenita.ribeiro@gmail.com>
2021-06-28 17:21:04 +1000

    final edits

--- plots/left_hemi/Figure8_shuffledFeatureMaps.py
+++ plots/left_hemi/Figure8_shuffledFeatureMaps.py
@@ -18,13 +18,13 @@ background = np.reshape(
 
 threshold = 1  # threshold for the curvature map
 
-# Background setting
+# Background settings
 nocurv = np.isnan(background)
 background[nocurv == 1] = 0
 background[background < 0] = 0
 background[background > 0] = 1
 
-# Setting the ROI
+# ROI settings
 label_primary_visual_areas = ['ROI']
 final_mask_L, final_mask_R, index_L_mask, index_R_mask = roi(
     label_primary_visual_areas)
@@ -32,12 +32,39 @@ final_mask_L, final_mask_R, index_L_mask, index_R_mask = roi(
 pred = np.zeros((32492, 1))
 measured = np.zeros((32492, 1))
 
-# Plotting the curvature and myelin values
+
 predictions = torch.load(
     '/home/uqfribe1/PycharmProjects/deepRetinotopy/testset_results/left_hemi'
     '/testset-shuffled-myelincurv_Model3_PA_LH.pt',
     map_location='cpu')
 
+pred[final_mask_L == 1] = np.reshape(
+    np.array(predictions['Predicted_values'][subject_index]),
+    (-1, 1))
+
+# Rescaling
+pred = np.array(pred)
+minus = pred > 180
+sum = pred < 180
+pred[minus] = pred[minus] - 180 + threshold
+pred[sum] = pred[sum] + 180 + threshold
+pred = np.array(pred)
+
+# Masking
+pred[final_mask_L != 1] = 0
+
+# Predicted map
+view = plotting.view_surf(
+    surf_mesh=osp.join(osp.dirname(osp.realpath(__file__)), '../..',
+                       'data/raw/original/S1200_7T_Retinotopy_9Zkk'
+                       '/S1200_7T_Retinotopy181/MNINonLinear/fsaverage_LR32k'
+                       '/S1200_7T_Retinotopy181.L.sphere.32k_fs_LR.surf.gii'),
+    surf_map=np.reshape(pred[0:32492], (-1)), bg_map=background,
+    cmap='gist_rainbow_r', black_bg=False, symmetric_cmap=False,
+    threshold=threshold, vmax=361)
+view.open_in_browser()
+
+# Plotting shuffled curvature and myelin values
 # Curvature
 pred[final_mask_L == 1] = np.reshape(
     np.array(predictions['Shuffled_curv'][subject_index]),
